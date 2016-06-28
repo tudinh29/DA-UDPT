c442cb0148bc1bcb251719de3d12ad1401ab671c
1312665 <1312665@student.hcmus.edu.vn>
2016-06-28 23:15:08 +0700

    Cap nhat router va page cssk

--- app/controllers/routes.js
+++ app/controllers/routes.js
@@ -15,10 +15,27 @@ module.exports = function(app, passport) {
     app.get('/phongkham', function(req, res) {
         res.render('phongkham.ejs');
     });
-    app.get('/suckhoe', function(req, res) {
+   app.get('/suckhoe', function(req, res) {
         res.render('cssk.ejs');
     });
-  
+    app.get('/suckhoe/food-new', function(req, res) {
+        res.render('food-new.ejs');
+    });
+    app.get('/suckhoe/food-most', function(req, res) {
+        res.render('food-most.ejs');
+    });
+    app.get('/suckhoe/health-new', function(req, res) {
+        res.render('health-new.ejs');
+    });
+    app.get('/suckhoe/health-most', function(req, res) {
+        res.render('health-most.ejs');
+    });
+    app.get('/suckhoe/cddd-new', function(req, res) {
+        res.render('cddd-new.ejs');
+    });
+    app.get('/suckhoe/cddd-most', function(req, res) {
+        res.render('cddd-most.ejs');
+    });
     app.get('/lichkham/lichsukham', function(req, res) {
         res.render('lichsukham.ejs');
     });
