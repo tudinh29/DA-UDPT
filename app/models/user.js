f1583901834ef69da568257241a2d0501fafb26f
htantruc <1312653@student.hcmus.edu.vn>
2016-06-29 07:34:53 +0700

    edit user

--- app/models/user.js
+++ app/models/user.js
@@ -35,7 +35,7 @@ userSchema.methods.generateHash = function(password) {
 
 // checking if password is valid
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
+    return bcrypt.compareSync(password, this.account.password);
 };
 
 // create the model for users and expose it to our app
