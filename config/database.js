4fbac2485e6080c552af5dcbc706a172a54dcef9
tudinh29 <1312676@student.hcmus.edu.vn>
2016-06-29 09:17:58 +0700

    Cap nhat lai danh sach phong kham, goi y

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://htantruc:daigiasv1@ds013162.mlab.com:13162/webchat/auth' // duong dan
+    'url' : 'mongodb://localhost:27017/DoAnUDPT' // duong dan
 
 };
