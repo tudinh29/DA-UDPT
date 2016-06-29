f1d92f8347924717eb6571aa4a658c4085700b06
htantruc <1312653@student.hcmus.edu.vn>
2016-06-29 07:24:16 +0700

    get+post đăng nhập, đăng kí

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,28 @@
+var User = require('./models/user.js');
 
 module.exports = function(app, passport) {
 	app.get('/', function(req, res) {
         res.render('index.ejs');
     });
-    app.get('/dangnhap', function(req, res) {
-        res.render('login.ejs');
+    // LOGIN =======================
+    app.get('/login', function(req, res) {
+        res.render('login.ejs', { message: req.flash('loginMessage') });
     });
-    app.get('/dangki', function(req, res) {
-        res.render('signup.ejs');
+    app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/lichkham', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+    }));
+    // SIGNUP ========================
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
     });
+    app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect : '/lichkham', // redirect to the secure profile section
+            failureRedirect : '/signup', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
     app.get('/lichkham', function(req, res) {
         res.render('lichkham.ejs');
     });
