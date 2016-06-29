e072078d72b23b6a599d1a6b781b01f82cec58c2
htantruc <1312653@student.hcmus.edu.vn>
2016-06-29 07:25:26 +0700

    thêm đường dẫn các trang khác

--- app/routes.js
+++ app/routes.js
@@ -35,44 +35,43 @@ module.exports = function(app, passport) {
             failureFlash : true // allow flash messages
         }));
 
-    app.get('/lichkham', function(req, res) {
-        res.render('lichkham.ejs');
+    app.get('/phongkham', isLoggedIn, function(req, res) {
+        res.render('phongkham.ejs',{
+        user : req.user
+        });
     });
-    app.get('/phongkham', function(req, res) {
-        res.render('phongkham.ejs');
+    app.get('/suckhoe', isLoggedIn, function(req, res) {
+        res.render('cssk.ejs',{
+        user : req.user
+        });
     });
-   app.get('/suckhoe', function(req, res) {
-        res.render('cssk.ejs');
+  
+    app.get('/lichkham/lichsukham', isLoggedIn, function(req, res) {
+        res.render('lichsukham.ejs',{
+        user : req.user
+        });
     });
-    app.get('/suckhoe/food-new', function(req, res) {
-        res.render('food-new.ejs');
+    app.get('/lichkham/themlichmoi', isLoggedIn, function(req, res) {
+        res.render('themlichmoi.ejs',{
+        user : req.user
+        });
     });
-    app.get('/suckhoe/food-most', function(req, res) {
-        res.render('food-most.ejs');
+    app.get('/phongkham/goiyphong', isLoggedIn, function(req, res) {
+        res.render('goiyphong.ejs',{
+        user : req.user
+        });
     });
-    app.get('/suckhoe/health-new', function(req, res) {
-        res.render('health-new.ejs');
-    });
-    app.get('/suckhoe/health-most', function(req, res) {
-        res.render('health-most.ejs');
-    });
-    app.get('/suckhoe/cddd-new', function(req, res) {
-        res.render('cddd-new.ejs');
-    });
-    app.get('/suckhoe/cddd-most', function(req, res) {
-        res.render('cddd-most.ejs');
-    });
-    app.get('/lichkham/lichsukham', function(req, res) {
-        res.render('lichsukham.ejs');
-    });
-    app.get('/lichkham/themlichmoi', function(req, res) {
-        res.render('themlichmoi.ejs');
-    });
-    app.get('/phongkham/goiyphong', function(req, res) {
-        res.render('goiyphong.ejs');
-    });
-    app.get('/phongkham/lichsuphong', function(req, res) {
-        res.render('lichsuphong.ejs');
+    app.get('/phongkham/lichsuphong', isLoggedIn, function(req, res) {
+        res.render('lichsuphong.ejs',{
+        user : req.user
+        });
     });
 
+}
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
 }
